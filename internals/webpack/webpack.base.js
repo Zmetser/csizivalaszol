a51becced850ca2d7de76e517df0a7047865033a
Oliver Kovacs <oliver@gizmodomedia.com>
2017-09-07 13:08:12 +0200

    chore(*): create web server for development

---
+++ internals/webpack/webpack.base.js
@@ -0,0 +1,93 @@
+/**
+ * COMMON WEBPACK CONFIGURATION
+ */
+
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = options => ({
+  context: path.join(process.cwd(), 'public'),
+  entry: options.entry,
+  output: Object.assign(
+    {
+      path: path.resolve(process.cwd(), 'build'),
+      publicPath: './dist'
+    },
+    options.output
+  ), // Merge with env dependent settings
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // Transform all .js files required somewhere with Babel
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: options.babelQuery
+      },
+      {
+        // Do not transform vendor's CSS with CSS-modules
+        // The point is that they remain in global scope.
+        // Since we require these CSS files in our JS or CSS files,
+        // they will be a part of our compilation either way.
+        // So, no need for ExtractTextPlugin here.
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loaders: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  plugins: (options.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      // make fetch available
+      fetch: 'exports-loader?self.fetch!whatwg-fetch'
+    }),
+
+    // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
+    // inside your code for any environment checks; UglifyJS will automatically
+    // drop any unreachable code.
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.NamedModulesPlugin()
+  ]),
+  resolve: {
+    modules: ['public', 'node_modules'],
+    extensions: ['.js'],
+    mainFields: ['browser', 'jsnext:main', 'main']
+  },
+  devtool: options.devtool,
+  target: 'web', // Make web variables accessible to webpack, e.g. window
+  performance: options.performance || {},
+  devServer: options.devServer || {}
+})
---
+++ internals/webpack/webpack.dev.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const webpack = require('webpack')
+// const CircularDependencyPlugin = require('circular-dependency-plugin')
+
+module.exports = require('./webpack.base')({
+  devServer: {
+    publicPath: '/build/',
+    contentBase: path.join(process.cwd(), 'public'),
+    compress: true,
+    port: 9000
+  },
+
+  entry: [
+    './app.js'
+  ],
+
+  // Don't use hashes in dev mode for better performance
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
+    path: path.resolve(process.cwd(), 'build')
+  },
+
+  // Add development plugins
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // Tell webpack we want hot reloading
+    new webpack.NoEmitOnErrorsPlugin(),
+    // new CircularDependencyPlugin({
+    //   exclude: /a\.js|node_modules/, // exclude node_modules
+    //   failOnError: false // show a warning when there is a circular dependency
+    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      children: true,
+      minChunks: 2,
+      async: true
+    })
+  ],
+
+  // Tell babel that we want to hot-reload
+  babelQuery: {
+    // require.resolve solves the issue of relative presets when dealing with
+    // locally linked packages. This is an issue with babel and webpack.
+    // See https://github.com/babel/babel-loader/issues/149 and
+    // https://github.com/webpack/webpack/issues/1866
+    presets: ['babel-preset-react-hmre'].map(require.resolve)
+  },
+
+  // Emit a source map for easier debugging
+  devtool: 'cheap-module-eval-source-map',
+
+  performance: {
+    hints: false
+  }
+})
