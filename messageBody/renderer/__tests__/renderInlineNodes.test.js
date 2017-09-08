a5fee71db5a3eb52a04df0467d5553524d06a4e0
Oliver Kovacs <oliver@gizmodomedia.com>
2017-09-08 12:04:58 +0200

    test(messageBody): write tests for inline node renderer

---
+++ messageBody/renderer/__tests__/renderInlineNodes.test.js
@@ -0,0 +1,43 @@
+/**
+ * @flow
+ */
+
+import * as React from 'react'
+import renderer from 'react-test-renderer'
+
+import { UnknownNodeError } from '../../errors'
+import renderInlineNodes from '../renderInlineNodes'
+
+import { createTextNode, createLink } from '../../nodes'
+
+const text = createTextNode('foo', [])
+const link = createLink([createTextNode('foo', [])], '#')
+
+describe('Render inline nodes', () => {
+  test('with an unknown element, should throw UnknownNodeError', () => {
+    // $FlowSkipForTesting: Testing what happens when a node is not supported.
+    const unknownNode = [{'type': 'Foo'}]
+    expect(() => renderInlineNodes(unknownNode)).toThrowError(UnknownNodeError)
+  })
+
+  test('with a single text node', () => {
+    const element = renderer.create(
+      <p>{renderInlineNodes([text])}</p>
+    )
+    expect(element.toJSON()).toMatchSnapshot()
+  })
+
+  test('with a single link', () => {
+    const element = renderer.create(
+      <p>{renderInlineNodes([link])}</p>
+    )
+    expect(element.toJSON()).toMatchSnapshot()
+  })
+
+  test('with everything', () => {
+    const element = renderer.create(
+      <p>{renderInlineNodes([text, link])}</p>
+    )
+    expect(element.toJSON()).toMatchSnapshot()
+  })
+})
--- messageBody/renderer/renderInlineNodes.js
+++ messageBody/renderer/renderInlineNodes.js
@@ -3,6 +3,7 @@
  */
 
 import * as React from 'react'
+import { UnknownNodeError } from '../errors'
 
 import type { InlineNode } from '../types'
 
@@ -11,12 +12,13 @@ import {
   Link
 } from '../components'
 
-function createInlineNode (node: InlineNode, index): ?(React.Element<any> | string) {
+function createInlineNode (node: InlineNode, index): React.Element<*> {
   switch (node.type) {
     case 'Text': return Text(node, index)
     case 'Link': return <Link {...node} key={index} />
+    default: throw new UnknownNodeError(node)
   }
 }
-export default function renderInlineNodes (nodes: Array<InlineNode>) {
+export default function renderInlineNodes (nodes: Array<InlineNode>): Array<React.Element<*>> {
   return nodes.map(createInlineNode)
 }
