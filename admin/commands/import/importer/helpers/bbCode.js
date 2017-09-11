4637eb2d36cc8b58a813e5ae507d4c4b010f2fd7
Oliver Kovacs <oliver@gizmodomedia.com>
2017-09-11 22:27:25 +0200

    fix(importer): bbCode regex for list and link

--- admin/commands/import/importer/helpers/bbCode.js
+++ admin/commands/import/importer/helpers/bbCode.js
@@ -7,9 +7,9 @@ module.exports = (text: string): string => {
     // BBcodes
     .replace(/(\[b\](.+?)\[\/b])/g, '<strong>$2</strong>')
     .replace(/\[size=10\]\[color=red\](.+?)\[\/color\]\[\/size\]/g, '')
-    .replace(/(\[url=(.+?)\])(.+?)(\[\/url\])/g, '<a href="$2" target="_blank">$3</li>')
-    .replace(/(\[url\])(.+?)(\[\/url\])/g, '<a href="$2" target="_blank">$2</li>')
+    .replace(/(\[url=(.+?)\])(.+?)(\[\/url\])/g, '<a href="$2" target="_blank">$3</a>')
+    .replace(/(\[url\])(.+?)(\[\/url\])/g, '<a href="$2" target="_blank">$2</a>')
     .replace(/\[list\](.+?)\[\/list\]/g, '<ul>$1</ul>')
     .replace(/\[list=a\](.+?)\[\/list\]/g, '<ul>$1</ul>')
-    .replace(/\[\*\](.+?)<\/br>/g, '<li>$2</li>')
+    .replace(/\[\*\](.+?)<br>/g, '<li>$1</li>')
 }
