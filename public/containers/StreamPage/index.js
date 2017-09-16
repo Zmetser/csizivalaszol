56d5ae33ac70dec8f4e4b8d59ad59c1e8341a072
Oliver Kovacs <oliver@gizmodomedia.com>
2017-09-16 21:54:49 +0200

    refactor(components/Entries): dumb Entries smart StreamPage

--- public/containers/StreamPage/index.js
+++ public/containers/StreamPage/index.js
@@ -1,19 +1,46 @@
 /**
  * @flow
  */
+import last from 'lodash/last'
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
 
 import Entries from '../../components/Entries'
 
-const AppBarExampleIcon = () => (
-  <div>
-    <AppBar
-      title='Csizi válaszol archívum'
-      showMenuIconButton={false}
-    />
-    <Entries />
-  </div>
-)
+import { getArchiveEntries, resolveEntries } from '../../api/entries'
+import type { EntryFull } from '../../types'
 
-export default AppBarExampleIcon
+type State = {
+  entries: Array<EntryFull>
+};
+
+export default class StreamPage extends React.Component<void, State> {
+  state = {
+    entries: []
+  };
+
+  componentDidMount () {
+    this.loadEntries()
+  }
+
+  loadEntries () {
+    const { entries } = this.state
+    const startAt = entries.length ? last(entries).id : null
+
+    getArchiveEntries(10, startAt)
+      .then(resolveEntries)
+      .then(entries => {
+        this.setState({ entries })
+      })
+  }
+
+  render () {
+    const { entries } = this.state
+    return (
+      <div>
+        <AppBar title='Csizi válaszol archívum' showMenuIconButton={false} />
+        <Entries entries={entries} />
+      </div>
+    )
+  }
+}
